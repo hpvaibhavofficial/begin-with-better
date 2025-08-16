29079501d30ca664df63213f01ab4d60ef503e48
Vaibhav Bhardwaj <hpvaibhavofficial@gmail.com>
2025-08-16 13:08:17 +0530

    16-08-2025

---
+++ Coursera/Web-Dev/JavaScript/test.js
@@ -0,0 +1,21 @@
+console.log("This is a test file for JavaScript in the Web-Dev directory.");
+var  a = 5;
+a=43
+var a = 111;
+let b = 10;
+b= 13
+env = "Development";
+const c = 12
+//c=122 // This will throw an error because 'c' is a constant
+console.log(a);
+console.log(b);
+console.log(env);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log (typeof env);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof NaN);    
+console.log(typeof console);
