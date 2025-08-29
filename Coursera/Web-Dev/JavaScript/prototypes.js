769152337b2597f98a7d0233a8c001cee0c9fc48
Vaibhav Bhardwaj <hpvaibhavofficial@gmail.com>
2025-08-30 02:24:00 +0530

    30-08-2025

--- Coursera/Web-Dev/JavaScript/prototypes.js
+++ Coursera/Web-Dev/JavaScript/prototypes.js
@@ -2,3 +2,10 @@ function createUser(username,score){
     this.username = username;
     this.score = score;
 }
+const u1 = new createUser("Vaibhav",23)
+createUser.prototype.hello = function(){
+    console.log(`Hello ${this.username}`);
+    
+}
+console.log(createUser.prototype);
+u1.hello()
