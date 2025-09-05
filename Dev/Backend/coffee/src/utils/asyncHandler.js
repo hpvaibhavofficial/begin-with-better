9236049add98e404e3cb657c32aa7acc0f0a9cd6
Vaibhav Bhardwaj <hpvaibhavofficial@gmail.com>
2025-09-05 12:27:27 +0530

    05-09-2025

--- Dev/Backend/coffee/src/utils/asyncHandler.js
+++ Dev/Backend/coffee/src/utils/asyncHandler.js
@@ -1,17 +1,62 @@
-// const asyncHandler = () => {}
-
-export default asyncHandler;
-
-const asyncHandler = (fn) => {
-    async (req,res,next) =>{
-        try{
-            await fn(req,res,next)
-        }
-        catch(e){
-            res.status(e).json({
-                success : true,
-                message : e.message
-            })
-        }
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch(err)
     }
 }
+
+
+
+ 
+// export default asyncHandler;
+
+// // const asyncHandler = (fn) => {
+// //     async (req,res,next) =>{
+// //         try{
+// //             await fn(req,res,next)
+// //         }
+// //         catch(e){
+// //             res.status(e).json({
+// //                 success : true,
+// //                 message : e.message
+// //             })
+// //         }
+// //     }
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
