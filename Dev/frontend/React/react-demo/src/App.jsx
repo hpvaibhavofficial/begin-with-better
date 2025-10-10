9530dd337e1fba7f7890f5d16d836b47a378f4ae
Vaibhav Bhardwaj <hpvaibhavofficial@gmail.com>
2025-10-10 17:23:41 +0530

    10-10-2025

--- Dev/frontend/React/react-demo/src/App.jsx
+++ Dev/frontend/React/react-demo/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import Welcome from './Welcome'
 
+function App({username = "nab" , password = "!1"}) {
+  let [count, setCount] = useState(5)
+  
+  const IncrementValue = () => {
+      setCount(count +1)
+  } 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  const decRement = () =>{
+    if(count <= 0){
+      setCount(0)
+    } else{
+      setCount(count-1)
+    }
+  }
   return (
     <>
-      HI i am react demo
-      <Welcome/>
+    <center>
+      <h1>Learning React : {username} is {password}</h1>
+      <h3>Counter Value : {count}</h3>
+      <button onClick={IncrementValue}>Increment</button>
+      <button onClick={decRement}>Decrement</button>
+    </center>
     </>
   )
 }
---
+++ Dev/frontend/React/todo-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <center><h1>Todo Demo</h1></center>
+    </>
+  )
+}
+
+export default App
